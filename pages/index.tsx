906439fb33311c86e6f12aab072954477f154b09
Sebastian Ferrari <sebas5384@gmail.com>
2020-01-17 17:09:02 -0300

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+import tw from 'tailwind.macro'
+
+const Title = styled.h1`
+  font-size: 50px;
+  ${tw`text-red`}
+`
+
+export default () => <Title>My page</Title>
