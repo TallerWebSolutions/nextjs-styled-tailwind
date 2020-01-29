e9528377142f2a172857aa74463a6ab7d80e809c
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2020-01-28 21:08:36 -0300

    Update to use Tailwind default class

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 import tw from 'tailwind.macro'
 
-const Title = styled.h1`
+const Title = styled('h1')`
   font-size: 50px;
-  ${tw`text-red`}
+  ${tw`text-red-500`}
 `
 
 export default () => <Title>My page</Title>
